8a8635fc061fb7ae06baa8e868c246d4fb5d2b9c
Christopher.Mazza <Christopher.Mazza@orleansasssessors.com>
2024-03-13 11:43:09 -0500

    add client files

--- opao-react/src/components/HorizontalSpacer/HorizontalSpacer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HorizontalSpacer = (props ) => {
-  return (
-    <div style={{marginLeft : props.space,marginRight : props.space}}></div>
-  )
-}
-
-export default HorizontalSpacer
--- opao-react/src/utils/navUtil.js
+++
@@ -1,6 +0,0 @@
-import {Navigate} from 'react-router-dom'
- 
-export const navigator = (path) => {
-
-    return <Navigate to ={path} />
-}
