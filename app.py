716b112d1e79cb5d823ea1b590333bb281e051a1
Christopher.Mazza <Christopher.Mazza@orleansasssessors.com>
2024-03-25 09:36:54 -0500

    still trying to fix the app.py file to work on server

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 activate_this = 'venv/bin/activate'
-exec(open(activate_this).read(), {'__file__': activate_this})
+with open(activate_this) as file_:
+    exec(file_.read(), {'__file__': activate_this})
+
 
 from src import create_app
 application = create_app()
