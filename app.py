c45f37f36b5c7d611210f0b359dd44c5f25054d2
Christopher.Mazza <Christopher.Mazza@orleansasssessors.com>
2024-03-25 09:32:49 -0500

    fix code in app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 activate_this = 'venv/bin/activate'
-exec(open(activate_this).read(), dict(__file__=activate_this))
+exec(open(activate_this).read(), {'__file__': activate_this})
 
 from src import create_app
-
 application = create_app()
