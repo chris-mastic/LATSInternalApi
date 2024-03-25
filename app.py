da756fca5b921b20e283c9e7ce51a363dbc2155e
Christopher.Mazza <Christopher.Mazza@orleansasssessors.com>
2024-03-25 13:04:32 -0500

    remove code to execute activate

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 #!/usr/local/bin/python3.10
-#
-
-activate_this = '/var/www/LATSInternalApi/venv/bin/activate'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
 
 from src import create_app
 application = create_app()
